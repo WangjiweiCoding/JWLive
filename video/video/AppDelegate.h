b4c58fc5d64905dc97baea9ce4122cc99b87611f
WangjiweiCoding <576709698@qq.com>
2016-12-05 17:35:42 +0800

    2016-12-05

---
+++ video/video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  video
+//
+//  Created by 王记伟 on 16/10/25.
+//  Copyright © 2016年 王记伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ video/video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by 王记伟 on 16/10/25.
+//  Copyright © 2016年 王记伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
