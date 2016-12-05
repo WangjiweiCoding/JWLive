5ee7dd3af43279423b49b3ff2a331641443df469
WangjiweiCoding <576709698@qq.com>
2016-12-05 18:03:28 +0800

    2016-12-05修改

--- video/video/AppDelegate.m
+++ video/video/AppDelegate.m
@@ -14,7 +14,10 @@
 
 @implementation AppDelegate
 
-
+- (void)df
+{
+    
+}
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
